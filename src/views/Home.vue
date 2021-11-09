9bdce6580fa9d48f8af6b5fc2b55b18498b32028
Diana Lagunovich <lagunnna.di@gmail.com>
2021-11-09 11:19:39 +0300

    KG-1, KG-3, KG-4, KG-5, KG-6, KG-7: init vue project

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    <h1>Hi, bitches <span style='font-size:2rem;'>&#128520;</span></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
