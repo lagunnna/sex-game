06936ba1b3828bfd581f17b9ac071869eb8467fe
Diana Lagunovich <lagunnna.di@gmail.com>
2021-11-23 22:44:04 +0300

    KG-9: rename var for better readability

--- src/views/SelectLevel.vue
+++ src/views/SelectLevel.vue
@@ -4,11 +4,11 @@
     <h1 class="title-levels">Уровень сложности</h1>
     <div class="btns-levels">
       <button
-      v-for="l of levels"
-      :key="l.id"
+      v-for="level of levels"
+      :key="level.id"
       class="active-btn active-btn-levels"
-      @click="() => setCurrentLevel(l)"
-      >{{l.title}}</button>
+      @click="() => setCurrentLevel(level)"
+      >{{level.title}}</button>
       <a href="https://docs.google.com/forms/d/e/1FAIpQLSdX8WJ8jmMZNloNDKaDJazrZ12qzvAbL58dvOgeHo6oPVyPow/viewform" target="_blank" rel="noopener noreferrer" class="active-btn-levels active-btn">Оставить отзыв</a>
     </div>
   </div>
