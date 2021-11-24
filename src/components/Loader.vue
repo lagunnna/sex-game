04efb433e7ded32c56e89d032e92e6d2b58ab920
Diana Lagunovich <drnnnnnn.a@gmail.com>
2021-11-24 21:34:37 +0300

    KG-11: add component Loader (#1)
    * KG-11: add component Loader
    
    * KG-11: add Loader to SelectLevel page
    
    * KG-11: remove the closure in the event
    
    Co-authored-by: programm1st <41444727+programm1st@users.noreply.github.com>

---
+++ src/components/Loader.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class = "loader"></div>
+</template>
+
+<script>
+export default {
+  name: 'Loader',
+};
+</script>
+
+<style lang="scss" scoped>
+.loader{
+  display: block;
+  margin: 3em auto;
+  width: 3em;
+  height: 3em;
+  border-radius: 50%;
+  border: 3px solid rgb(0, 180, 222, 0.15);
+  border-top-color: #00b4de;
+  animation: spin 1s ease-in-out infinite;
+
+  @media (max-width: 50em) {
+    font-size: 2rem;
+    }
+}
+
+@keyframes spin{
+  to{
+    transform: rotate(360deg);
+  }
+}
+
+</style>
