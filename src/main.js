1ddd9cd186ea8d682b548addd0eccb4b6bc4570e
Diana Lagunovich <lagunnna.di@gmail.com>
2021-12-06 19:56:00 +0300

    KG-12: add firebase database

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuelidate from 'vuelidate';
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
 import { initializeApp } from 'firebase/app';
+import 'firebase/database';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import App from './App.vue';
 import Loader from './components/Loader.vue';
@@ -19,20 +20,21 @@ Vue.use(BootstrapVueIcons);
 
 Vue.component('Loader', Loader);
 
-initializeApp({
-  apiKey: 'AIzaSyDOSIFVnr4OXhYax396UHHMsKGaHczG3mc',
-  authDomain: 'duo-sex-game.firebaseapp.com',
-  projectId: 'duo-sex-game',
-  storageBucket: 'duo-sex-game.appspot.com',
-  messagingSenderId: '166262890225',
-  appId: '1:166262890225:web:a461aaf722b297cf469350',
-  measurementId: 'G-TDNE70S15W',
+const firebaseApp = initializeApp({
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID,
 });
 
-const auth = getAuth();
+const firebaseAuth = getAuth(firebaseApp);
 let app;
 
-onAuthStateChanged(auth, () => {
+onAuthStateChanged(firebaseAuth, () => {
   if (!app) {
     new Vue({
       router,
